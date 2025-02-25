e803ed7e0bc6f1b2a2044e6a1ce684c6441c49ad
AdelAlmutairii <adil.bioinfo@gmail.com>
2025-02-26 00:29:24 +0300

    Update for_loops.py

--- for_loops.py
+++ for_loops.py
@@ -1,10 +1,12 @@
-students_scores = []
+# Makig max() function
+
+students_scores = [] # Create an empty list 
 for i in range(200):
     students_scores.append(i)
 print(students_scores)
 
-max_score = 0
+max_score = 0 # Create an empty counter to hold the largest number
 for score in students_scores:
     if score > max_score:
-        max_score = score
+        max_score = score # re assign the variable to the largest number you checked
 print(max_score)
